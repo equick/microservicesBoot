719a51480a18f55bc0b205da07aba759799943bc
equick <edwardquick@hotmail.com>
2016-08-02 16:24:51 +0100

    Corrected spelling of position

--- src/main/java/demo/domain/Player.java
+++ src/main/java/demo/domain/Player.java
@@ -10,16 +10,16 @@ public class Player {
 	@Id @GeneratedValue
 	Long id;
 	String name;
-	String postion;
+	String position;
 	
 	public Player() {
 		super();
 	}
 	
-	public Player(String name, String postion) {
+	public Player(String name, String position) {
 		this();
 		this.name = name;
-		this.postion = postion;
+		this.position = position;
 	}
 	
 	public String getName() {
@@ -28,11 +28,11 @@ public class Player {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getPostion() {
-		return postion;
+	public String getPosition() {
+		return position;
 	}
-	public void setPostion(String postion) {
-		this.postion = postion;
+	public void setPosition(String position) {
+		this.position = position;
 	}
 	
 	
